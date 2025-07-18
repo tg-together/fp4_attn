corrected index overflow in kernel and added global scale

eval.py
@@ -204,12 +204,12 @@ def main():
     if args.quantize:
         llama_fp4_attention_forward.quantize_enabled = args.quantize
 
-    if args.quantize or args.visualize:
-        patch_attention()   ## Enable FP4 attention
+    # if args.quantize or args.visualize:
+    patch_attention()   ## Enable FP4 attention
 
     # start_record_memory_history()
 
-    for max_length in [512, 2048, 4096]:
+    for max_length in [2048]:
         with torch.no_grad():
             results = calculate_perplexity(
                 model=args.model,

fp4_quant_utils.py
@@ -11,6 +11,7 @@ import math
 from torch import nn, Tensor
 from typing import Tuple
 import nvfp4sim
+import time
 
 
 class FP4Quantizer(nn.Module):
@@ -19,24 +20,26 @@ class FP4Quantizer(nn.Module):
     def __init__(self, 
                  block_size: int,
                  float4_e2m1_max: float,
+                 float8_e4m3_max: float,
                  global_sf: float,
                  dequant_dtype: torch.dtype = torch.bfloat16,
-                 use_kernel: bool = False):
+                 use_search: bool = False):
         super().__init__()
         self.block_size = block_size
         self.float4_e2m1_max = torch.tensor(float4_e2m1_max, dtype=dequant_dtype)
+        self.float8_e4m3_max = torch.tensor(float8_e4m3_max, dtype=dequant_dtype)
         self.global_sf = torch.tensor(global_sf, dtype=dequant_dtype)
         self.zero_tensor = torch.tensor(0.0, dtype=dequant_dtype)
         self.one_tensor = torch.tensor(1.0, dtype=dequant_dtype)
         self.dequant_dtype = dequant_dtype
-        self.use_kernel = use_kernel
+        self.use_search = use_search
 
         # print(f"FP4Quantizer initialized with:")
         # print(f"block_size: {block_size}")
         # print(f"float4_e2m1_max: {float4_e2m1_max}")
         # print(f"global_sf: {global_sf}")
         # print(f"dequant_dtype: {dequant_dtype}")
-        # print(f"use_search: {use_kernel}")
+        # print(f"use_search: {use_search}")
 
     
     def cast_to_fp4(self, x: Tensor) -> Tensor:
@@ -79,7 +82,7 @@ class FP4Quantizer(nn.Module):
     #         x = torch.nn.functional.pad(x, (0, pad_size), value=0.0)
     #         n = x.shape[1]
 
-    #     x_3d = x.reshape(m, n // self.block_size, self.block_size)
+    #     x_3d = x.reshape(m, n // self.block_size, self.block_size) n 
 
     #     # Find max absolute value per block
     #     vec_max = torch.max(torch.abs(x_3d), dim=-1, keepdim=True)[0].to(self.dequant_dtype)
@@ -104,11 +107,12 @@ class FP4Quantizer(nn.Module):
     #     return q, scale.squeeze(-1)
 
 
-    def single_nvfp4_quant_dequant_fused(self, x: Tensor, global_sf: float = None) -> Tuple[Tensor, Tensor]:
+    def single_nvfp4_quant_dequant_fused(self, x: Tensor, global_sf: float = None, global_scale_aligned : bool = True):
         """Block-float nvFP4 PTQ (single slice)."""
-        if global_sf is None:
-            global_sf = self.global_sf
-            
+        torch.cuda.empty_cache()
+        x = x.to(self.dequant_dtype)
+  
+        
         assert x.ndim == 2
         m, n = x.shape
         n_orig = n
@@ -120,29 +124,42 @@ class FP4Quantizer(nn.Module):
             x = torch.nn.functional.pad(x, (0, pad_size), value=0.0)
             n = x.shape[1]
 
+        if global_scale_aligned:
+            global_sf = torch.max(abs(x), dim=1, keepdim=True)[0].to(self.dequant_dtype)
+        else:
+            global_sf = torch.max(abs(x), dim=0, keepdim=True)[0].to(self.dequant_dtype)
+
+        global_sf = global_sf * self.get_reciprocal(self.float4_e2m1_max*self.float8_e4m3_max)
+
+        x=x*self.get_reciprocal(global_sf)
+
+
         x = x.reshape(m, n // self.block_size, self.block_size)
 
         # Find max absolute value per block
-        scale = torch.max(torch.abs(x), dim=-1, keepdim=True)[0].to(self.dequant_dtype)
+        scale = torch.max(torch.abs(x), dim=-1, keepdim=True)[0]
 
         # Calculate scale factor per block
-        scale = global_sf * (scale * self.get_reciprocal(self.float4_e2m1_max))
+        scale = (scale)* self.get_reciprocal(self.float4_e2m1_max)
         # Quantize scale to FP8 for realistic simulation
+
         scale = scale.to(torch.float8_e4m3fn).to(self.dequant_dtype)
 
-        # Calculate output scale factor (reciprocal for dequantization)
-        out_sf = self.get_reciprocal(scale * self.get_reciprocal(global_sf))
+        
 
         # Scale input into FP4 range and quantize
-        x = x.to(self.dequant_dtype) * out_sf
+        x = x * self.get_reciprocal(scale) 
         x = torch.clamp(x, -self.float4_e2m1_max, self.float4_e2m1_max)
         x = self.cast_to_fp4(x)
-        x = x * scale / global_sf
-        x = x.reshape(m, n)
+        x = x * scale
+        x = x.view(m, n)
+
+        x=x*global_sf
         # Return original size if we padded
         if n != n_orig:
             x = x[:, :n_orig]
-
+        
+        torch.cuda.empty_cache()
         return x 
 
     # def single_nvfp4_dequant(self, q: Tensor, scale: Tensor, global_sf: float = None) -> Tensor:
@@ -169,10 +186,9 @@ class FP4Quantizer(nn.Module):
     #         result = result[:, :orig_n]
 
     #     return result
+    def single_nvfp4_qd_nosearch(self, x:Tensor, global_sf: float = None, global_scale_aligned : bool = True):
 
-    def single_nvfp4_qd_searched(self, x:Tensor, global_sf: float = None):
-
-        
+        torch.cuda.empty_cache()
 
         x=x.to(torch.float32)
 
@@ -187,6 +203,73 @@ class FP4Quantizer(nn.Module):
             n = x.shape[1]
             m = x.shape[0]
         x=x.contiguous()
+
+        if global_scale_aligned:
+            global_sf = torch.max(abs(x), dim=1, keepdim=True)[0].to(self.dequant_dtype)
+        else:
+            global_sf = torch.max(abs(x), dim=0, keepdim=True)[0].to(self.dequant_dtype)
+
+        global_sf = global_sf * self.get_reciprocal(self.float4_e2m1_max*self.float8_e4m3_max)
+
+        x=x*self.get_reciprocal(global_sf)
+
+        x = x.view(m * (n // self.block_size), self.block_size)
+
+
+        M = x.shape[0]  # total number of groups
+
+
+        quantized_data = torch.empty(M, dtype=torch.int64, device=x.device)
+        scales = torch.empty(M, dtype=torch.float8_e4m3fn, device=x.device)
+        
+
+        
+        nvfp4sim.f32_to_nvf4_nosearch(quantized_data, scales, x)
+
+
+
+        reconstructed_f32 = torch.empty(m * (n // self.block_size), self.block_size, dtype=torch.float32, device=x.device)
+        nvfp4sim.nvf4_to_f32(reconstructed_f32, quantized_data, scales)
+
+        # 
+
+        reconstructed_f32 = reconstructed_f32.view(m, n)
+        reconstructed_f32=reconstructed_f32*global_sf
+
+        if n != n_orig or m != m_orig:
+            reconstructed_f32 = reconstructed_f32[:m_orig, :n_orig]
+
+        torch.cuda.empty_cache()
+        return reconstructed_f32.to(torch.bfloat16)
+
+
+    def single_nvfp4_qd_searched(self, x:Tensor, global_sf: float = None, global_scale_aligned : bool = True):
+
+        torch.cuda.empty_cache()
+
+        x=x.to(torch.float32)
+
+        m_orig, n_orig = x.shape
+        n=n_orig
+        m=m_orig
+
+        if n_orig % self.block_size != 0 or m_orig % 2 != 0:
+            pad_cols = self.block_size - (n_orig % self.block_size) if n_orig % self.block_size != 0 else 0
+            pad_rows = 1 if m_orig % 2 != 0 else 0
+            x = torch.nn.functional.pad(x, (0, pad_cols, 0, pad_rows), value=0.0)
+            n = x.shape[1]
+            m = x.shape[0]
+        x=x.contiguous()
+
+        if global_scale_aligned:
+            global_sf = torch.max(abs(x), dim=1, keepdim=True)[0].to(self.dequant_dtype)
+        else:
+            global_sf = torch.max(abs(x), dim=0, keepdim=True)[0].to(self.dequant_dtype)
+
+        global_sf = global_sf * self.get_reciprocal(self.float4_e2m1_max*self.float8_e4m3_max)
+
+        x=x*self.get_reciprocal(global_sf)
+
         x = x.view(m * (n // self.block_size), self.block_size)
 
 
@@ -205,43 +288,32 @@ class FP4Quantizer(nn.Module):
         reconstructed_f32 = torch.empty(m * (n // self.block_size), self.block_size, dtype=torch.float32, device=x.device)
         nvfp4sim.nvf4_to_f32(reconstructed_f32, quantized_data, scales)
 
-
-        del quantized_data
-        del scales
-        del x
-        torch.cuda.empty_cache()
-
+        # 
 
         reconstructed_f32 = reconstructed_f32.view(m, n)
+        reconstructed_f32=reconstructed_f32*global_sf
 
         if n != n_orig or m != m_orig:
             reconstructed_f32 = reconstructed_f32[:m_orig, :n_orig]
 
-        
+        torch.cuda.empty_cache()
         return reconstructed_f32.to(torch.bfloat16)
 
-    def dual_nvfp4_fake_quant(self, x: Tensor, global_sf: float = None):
+    def dual_nvfp4_fake_quant(self, x: Tensor, global_sf: float = None, global_scale_aligned : bool = True):
         """Return (q_hi, s_hi, q_lo, s_lo) such that
            x ≈ s_hi · q_hi + s_lo · q_lo
            Both q_hi and q_lo are nvFP4 tensors.
         """
-        if global_sf is None:
-            global_sf = self.global_sf
-            
 
-        if not self.use_kernel:
-            x_hi_reconstructed = self.single_nvfp4_quant_dequant_fused(x, global_sf) #self.single_nvfp4_dequant(q_hi, s_hi, global_sf)
-            x_residual = x - x_hi_reconstructed
-            x_low_reconstructed = self.single_nvfp4_quant_dequant_fused(x_residual, global_sf)
-            del x_residual
+        if not self.use_search:
+            x_hi_reconstructed = self.single_nvfp4_qd_nosearch(x, global_sf, global_scale_aligned) #self.single_nvfp4_dequant(q_hi, s_hi, global_sf)
+            return x_hi_reconstructed + self.single_nvfp4_qd_nosearch(x - x_hi_reconstructed, global_sf, global_scale_aligned)
+
         else:
-            x_hi_reconstructed = self.single_nvfp4_qd_searched(x, global_sf) 
-            x_residual = x - x_hi_reconstructed
-            x_low_reconstructed = self.single_nvfp4_qd_searched(x_residual, global_sf)
-            del x_residual
-        torch.cuda.empty_cache()
-        return x_hi_reconstructed+x_low_reconstructed
-    
+            x_hi_reconstructed = self.single_nvfp4_qd_searched(x, global_sf, global_scale_aligned) 
+            return x_hi_reconstructed + self.single_nvfp4_qd_searched(x - x_hi_reconstructed, global_sf, global_scale_aligned)
+
+        
     # def dual_nvfp4_fake_quant(self, x: Tensor, global_sf: float = None):
     #     """Dual-slice FP4 quantization with fake quantization."""
     #     if global_sf is None:
@@ -256,12 +328,11 @@ class FP4Quantizer(nn.Module):
     #     xd = xh + xl
 
     #     return xd
-    def single_nvfp4_fake_quant(self, x: Tensor, global_sf: float = None):
- 
-        if global_sf is None:
-            global_sf = self.global_sf
-        if not self.use_kernel:
-            return self.single_nvfp4_quant_dequant_fused(x, global_sf)
+    def single_nvfp4_fake_quant(self, x: Tensor, global_sf: float = None, global_scale_aligned : bool = True):
+
+        if not self.use_search:
+            return self.single_nvfp4_qd_nosearch(x, global_sf, global_scale_aligned)
         else:
-            return self.single_nvfp4_qd_searched(x, global_sf)
+            return self.single_nvfp4_qd_searched(x, global_sf, global_scale_aligned)
+
 

llama_patch.py
@@ -73,8 +73,8 @@ def eager_attention_forward(
         causal_mask = attention_mask[:, :, :, : key_states.shape[-2]]
         attn_weights = attn_weights + causal_mask
 
-
     attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query.dtype)
+
     attn_weights = nn.functional.dropout(attn_weights, p=dropout, training=module.training)
 
  
@@ -97,6 +97,7 @@ def eager_attention_forward(
             
             # Reshape back to original shape
             attn_weights = attn_weights_2d.view(original_shape)
+
 
     attn_output = torch.matmul(attn_weights, value_states)
     attn_output = attn_output.transpose(1, 2).contiguous()
@@ -122,17 +123,19 @@ def llama_fp4_attention_forward(
         inferred_dtype = self.q_proj.weight.dtype
         
         # Get environment variables for configuration
-        use_kernel = os.getenv('FP4_USE_KERNEL', 'false').lower() == 'true'
+        use_search = os.getenv('FP4_USE_SEARCH', 'false').lower() == 'true'
         
         # Initialize FP4Quantizer with appropriate parameters
         self.fp4_quantizer = FP4Quantizer(
             block_size=16,  # Common block size for attention quantization
             float4_e2m1_max=6.0,  # Max value for FP4 E2M1 format
+            float8_e4m3_max=448.0,  # Max value for FP8 E4M3 format
             global_sf=0.5,  # Global scale factor
             dequant_dtype=inferred_dtype,
-            use_kernel=use_kernel
+            use_search=use_search
         )
         self.fp4_quantizer.float4_e2m1_max = self.fp4_quantizer.float4_e2m1_max.to(device)
+        self.fp4_quantizer.float8_e4m3_max = self.fp4_quantizer.float8_e4m3_max.to(device)
         self.fp4_quantizer.global_sf = self.fp4_quantizer.global_sf.to(device)
         self.fp4_quantizer.zero_tensor = self.fp4_quantizer.zero_tensor.to(device)
         self.fp4_quantizer.one_tensor = self.fp4_quantizer.one_tensor.to(device)
@@ -182,12 +185,16 @@ def llama_fp4_attention_forward(
             # start_time = time.time_ns()
             # Apply quantization
             if use_dual_quant_q:
+                
                 q2d = self.fp4_quantizer.dual_nvfp4_fake_quant(q2d)
             else:
+                
                 q2d = self.fp4_quantizer.single_nvfp4_fake_quant(q2d)
             
             k2d = self.fp4_quantizer.single_nvfp4_fake_quant(k2d)
-            v2d = self.fp4_quantizer.single_nvfp4_fake_quant(v2d.T).T
+            
+            v2d = self.fp4_quantizer.single_nvfp4_fake_quant(v2d.T, global_scale_aligned=False).T
+            
             # print(f"Time taken: {(time.time_ns() - start_time)/1e6:.4f} ms\n")
 
             # Collect differences if visualizing
@@ -212,10 +219,18 @@ def llama_fp4_attention_forward(
 
     ##### UNCOMMENT THIS FOR EAGER ATTENTION AND P QUANTIZATION #####
 
-    # self.config._attn_implementation = "eager"
+    self.config._attn_implementation = "eager"
 
     if self.config._attn_implementation != "eager":
         attention_interface = ALL_ATTENTION_FUNCTIONS[self.config._attn_implementation]
+
+
+    if torch.isnan(key_states).any() or torch.isnan(value_states).any() or torch.isnan(query_states).any():
+        print("K nan:", torch.isnan(key_states).any())
+        print("V nan:", torch.isnan(value_states).any())
+        print("Q nan:", torch.isnan(query_states).any())
+        raise
+
 
 
     attn_output, attn_weights = attention_interface(