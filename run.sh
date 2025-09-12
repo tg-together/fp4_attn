4da3d30581b2aa9f3eedca91ac080f41806a0be8
Tanmaey Gupta <tanmaey@research-common-h200-02.cloud.together.ai>
2025-09-12 15:12:58 +0000

    initial commit

--- run.sh
+++ run.sh
@@ -13,22 +13,8 @@
 export PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True"
 
 
-num_samples=2500
+num_samples=100
 
 
-CUDA_VISIBLE_DEVICES=0 FP4_USE_DUAL_QUANT_Q=false FP4_USE_Q_SEARCH=false FP4_USE_DUAL_QUANT_ATTN=true FP4_USE_P_SEARCH=true SHIFTED_SM=true python eval.py --device cuda:0 --model meta-llama/Meta-Llama-3-8B --task pile_10k --num_samples $num_samples --quantize KVP 2>&1 | tee logs/Q_not_quantized.txt &
-
-CUDA_VISIBLE_DEVICES=1 FP4_USE_DUAL_QUANT_Q=false FP4_USE_Q_SEARCH=true FP4_USE_DUAL_QUANT_ATTN=true FP4_USE_P_SEARCH=true SHIFTED_SM=true python eval.py --device cuda:1 --model meta-llama/Meta-Llama-3-8B --task pile_10k --num_samples $num_samples --quantize QKVP 2>&1 | tee logs/Q_single_search.txt &
-
-CUDA_VISIBLE_DEVICES=2 FP4_USE_DUAL_QUANT_Q=true FP4_USE_Q_SEARCH=false FP4_USE_DUAL_QUANT_ATTN=true FP4_USE_P_SEARCH=true SHIFTED_SM=true python eval.py --device cuda:2 --model meta-llama/Meta-Llama-3-8B --task pile_10k --num_samples $num_samples --quantize QKVP 2>&1 | tee logs/Q_dual_nosearch.txt &
-
-CUDA_VISIBLE_DEVICES=3 FP4_USE_DUAL_QUANT_Q=true FP4_USE_Q_SEARCH=true FP4_USE_DUAL_QUANT_ATTN=true FP4_USE_P_SEARCH=true SHIFTED_SM=true python eval.py --device cuda:3 --model meta-llama/Meta-Llama-3-8B --task pile_10k --num_samples $num_samples --quantize QKVP 2>&1 | tee logs/Q_dual_search.txt &
-
-CUDA_VISIBLE_DEVICES=4 FP4_USE_DUAL_QUANT_Q=true FP4_USE_Q_SEARCH=true ZERO_POINT=mean FP4_USE_DUAL_QUANT_ATTN=true FP4_USE_P_SEARCH=true SHIFTED_SM=true python eval.py --device cuda:4 --model meta-llama/Meta-Llama-3-8B --task pile_10k --num_samples $num_samples --quantize QKVP 2>&1 | tee logs/Q_dual_search_mean_zp.txt &
-
-CUDA_VISIBLE_DEVICES=5 FP4_USE_DUAL_QUANT_Q=true FP4_USE_Q_SEARCH=true ZERO_POINT=min FP4_USE_DUAL_QUANT_ATTN=true FP4_USE_P_SEARCH=true SHIFTED_SM=true python eval.py --device cuda:5 --model meta-llama/Meta-Llama-3-8B --task pile_10k --num_samples $num_samples --quantize QKVP 2>&1 | tee logs/Q_dual_search_min_zp.txt &
-
-CUDA_VISIBLE_DEVICES=6 FP4_USE_DUAL_QUANT_Q=false FP4_USE_Q_SEARCH=true ZERO_POINT=mean FP4_USE_DUAL_QUANT_ATTN=true FP4_USE_P_SEARCH=true SHIFTED_SM=true python eval.py --device cuda:6 --model meta-llama/Meta-Llama-3-8B --task pile_10k --num_samples $num_samples --quantize QKVP 2>&1 | tee logs/Q_single_search_mean_zp.txt &
-
-CUDA_VISIBLE_DEVICES=7 FP4_USE_DUAL_QUANT_Q=false FP4_USE_Q_SEARCH=true ZERO_POINT=min FP4_USE_DUAL_QUANT_ATTN=true FP4_USE_P_SEARCH=true SHIFTED_SM=true python eval.py --device cuda:7 --model meta-llama/Meta-Llama-3-8B --task pile_10k --num_samples $num_samples --quantize QKVP 2>&1 | tee logs/Q_single_search_min_zp.txt &
+CUDA_VISIBLE_DEVICES=0 MEAN_BEFORE_ROPE=true python eval.py --device cuda:0 --model meta-llama/Meta-Llama-3-8B --task pile_10k --num_samples $num_samples --quantize QKVP 2>&1 | tee temp.txt
 
