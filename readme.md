62817ef2c52bf836d0f61db99f449d9cfedcc78b
Tanmaey Gupta <tanmaey@research-common-h200-02.cloud.together.ai>
2025-09-12 16:02:53 +0000

    udpated setup.sh, readme

--- readme.md
+++ readme.md
@@ -1,9 +1,3 @@
-Setup:
-
-pip install lm_eval
-
-cd fast_fp4
-
-pip install -e . / python setup.py build_ext --inplace
+bash setup.sh
 
 bash run.sh
--- setup.sh
+++ setup.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 set -e  # exit on error
 
-# Initialize conda
-eval "$(conda shell.bash hook)"
+# # Initialize conda
+# eval "$(conda shell.bash hook)"
 
-# Create env
-conda create -n fp4 python=3.12 -y
+# # Create env
+# conda create -n fp4 python=3.12 -y
 
-# Activate env
-conda activate fp4
+# # Activate env
+# conda activate fp4
 
-# Install packages
-pip install lm_eval matplotlib
+# # Install packages
+# pip install lm_eval matplotlib
 
+
+BASHRC="$HOME/.bashrc"
+
+# Remove old CUDA lines (if any)
+sed -i '/export CUDA_HOME=\/usr\/local\/cuda/d' "$BASHRC"
+sed -i '/export PATH=\$CUDA_HOME\/bin:\$PATH/d' "$BASHRC"
+sed -i '/export LD_LIBRARY_PATH=\$CUDA_HOME\/lib64:\$LD_LIBRARY_PATH/d' "$BASHRC"
+
+# Add new CUDA 12.9 lines
+cat << 'EOF' >> "$BASHRC"
+
+# >>> CUDA 12.9 default setup >>>
+export CUDA_HOME=/usr/local/cuda
+export PATH=$CUDA_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+# <<< CUDA 12.9 default setup <<<
+EOF
+
+source "$BASHRC"
 # Install local package if directory exists
 if [ -d "fast_fp4" ]; then
   cd fast_fp4
