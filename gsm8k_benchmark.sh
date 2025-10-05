bf2d86ff46b542bb17534534cc518bc7e5607336
Tanmaey Gupta <tanmaey@research-common-h200-02.cloud.together.ai>
2025-10-05 22:02:02 +0000

    corrected gsm8k task for llama

--- gsm8k_benchmark.sh
+++ gsm8k_benchmark.sh
@@ -8,7 +8,7 @@ TASK="gsm8k_cot"
 NUM_FEWSHOT="8"
 
 # Common suffix for all commands
-SUFFIX_CMD="--task ${TASK} --apply_chat_template --num_fewshot ${NUM_FEWSHOT} --fewshot_as_multiturn --log_samples"
+SUFFIX_CMD="--apply_chat_template --num_fewshot ${NUM_FEWSHOT} --fewshot_as_multiturn --log_samples"
 
 export PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True"
 export HF_HOME="/scratch/huggingface"
@@ -21,16 +21,16 @@ if [ "$MODE" = "baseline" ] || [ "$MODE" = "all" ]; then
     
 
     MODEL="meta-llama/Llama-3.2-3B-Instruct"
-    CUDA_VISIBLE_DEVICES=4  python lmeval_main.py --model "${MODEL}" ${SUFFIX_CMD} --output "./logs/gsm8k_benchmark/baseline/${MODEL##*/}_gsm8k.jsonl" 2>&1 | tee "logs/gsm8k_benchmark/baseline/${MODEL##*/}_gsm8k.txt" &
+    CUDA_VISIBLE_DEVICES=4  python lmeval_main.py --model "${MODEL}" ${SUFFIX_CMD} --output "./logs/gsm8k_benchmark/baseline/${MODEL##*/}_gsm8k.jsonl" --task gsm8k_cot_llama 2>&1 | tee "logs/gsm8k_benchmark/baseline/${MODEL##*/}_gsm8k.txt" &
     
     MODEL="deepseek-ai/DeepSeek-R1-0528-Qwen3-8B"
-    CUDA_VISIBLE_DEVICES=5  python lmeval_main.py --model "${MODEL}" ${SUFFIX_CMD} --output "./logs/gsm8k_benchmark/baseline/${MODEL##*/}_gsm8k.jsonl" 2>&1 | tee "logs/gsm8k_benchmark/baseline/${MODEL##*/}_gsm8k.txt" &
+    CUDA_VISIBLE_DEVICES=5  python lmeval_main.py --model "${MODEL}" ${SUFFIX_CMD} --output "./logs/gsm8k_benchmark/baseline/${MODEL##*/}_gsm8k.jsonl" --task gsm8k_cot 2>&1 | tee "logs/gsm8k_benchmark/baseline/${MODEL##*/}_gsm8k.txt" &
     
     # MODEL="meta-llama/Llama-3.3-70B-Instruct"
-    # CUDA_VISIBLE_DEVICES=6  python lmeval_main.py --model "${MODEL}" ${SUFFIX_CMD} --output "./logs/gsm8k_benchmark/baseline/${MODEL##*/}_gsm8k.jsonl" 2>&1 | tee "logs/gsm8k_benchmark/baseline/${MODEL##*/}_gsm8k.txt" &
+    # CUDA_VISIBLE_DEVICES=6  python lmeval_main.py --model "${MODEL}" ${SUFFIX_CMD} --output "./logs/gsm8k_benchmark/baseline/${MODEL##*/}_gsm8k.jsonl" --task gsm8k_cot_llama 2>&1 | tee "logs/gsm8k_benchmark/baseline/${MODEL##*/}_gsm8k.txt" &
     
     MODEL="Qwen/Qwen3-4B-Thinking-2507"
-    CUDA_VISIBLE_DEVICES=7  python lmeval_main.py --model "${MODEL}" ${SUFFIX_CMD} --output "./logs/gsm8k_benchmark/baseline/${MODEL##*/}_gsm8k.jsonl" 2>&1 | tee "logs/gsm8k_benchmark/baseline/${MODEL##*/}_gsm8k.txt" &
+    CUDA_VISIBLE_DEVICES=7  python lmeval_main.py --model "${MODEL}" ${SUFFIX_CMD} --output "./logs/gsm8k_benchmark/baseline/${MODEL##*/}_gsm8k.jsonl" --task gsm8k_cot 2>&1 | tee "logs/gsm8k_benchmark/baseline/${MODEL##*/}_gsm8k.txt" &
 
 elif [ "$MODE" = "fp4" ] || [ "$MODE" = "all" ]; then
     mkdir -p "logs/gsm8k_benchmark/fp4/"
@@ -38,16 +38,16 @@ elif [ "$MODE" = "fp4" ] || [ "$MODE" = "all" ]; then
     #FP4
     
     MODEL="meta-llama/Llama-3.2-3B-Instruct"
-    CUDA_VISIBLE_DEVICES=0  python lmeval_main.py --model "${MODEL}" ${SUFFIX_CMD} --quantize --output "./logs/gsm8k_benchmark/fp4/${MODEL##*/}_gsm8k.jsonl" 2>&1 | tee "logs/gsm8k_benchmark/fp4/${MODEL##*/}_gsm8k.txt" &
+    CUDA_VISIBLE_DEVICES=0  python lmeval_main.py --model "${MODEL}" ${SUFFIX_CMD} --quantize --output "./logs/gsm8k_benchmark/fp4/${MODEL##*/}_gsm8k.jsonl" --task gsm8k_cot_llama 2>&1 | tee "logs/gsm8k_benchmark/fp4/${MODEL##*/}_gsm8k.txt" &
     
     MODEL="deepseek-ai/DeepSeek-R1-0528-Qwen3-8B"
-    CUDA_VISIBLE_DEVICES=1  python lmeval_main.py --model "${MODEL}" ${SUFFIX_CMD} --quantize --output "./logs/gsm8k_benchmark/fp4/${MODEL##*/}_gsm8k.jsonl" 2>&1 | tee "logs/gsm8k_benchmark/fp4/${MODEL##*/}_gsm8k.txt" &
+    CUDA_VISIBLE_DEVICES=1  python lmeval_main.py --model "${MODEL}" ${SUFFIX_CMD} --quantize --output "./logs/gsm8k_benchmark/fp4/${MODEL##*/}_gsm8k.jsonl" --task gsm8k_cot 2>&1 | tee "logs/gsm8k_benchmark/fp4/${MODEL##*/}_gsm8k.txt" &
     
     # MODEL="meta-llama/Llama-3.3-70B-Instruct"
-    # CUDA_VISIBLE_DEVICES=6  python lmeval_main.py --model "${MODEL}" ${SUFFIX_CMD} --quantize --output "./logs/gsm8k_benchmark/fp4/${MODEL##*/}_gsm8k.jsonl" 2>&1 | tee "logs/gsm8k_benchmark/fp4/${MODEL##*/}_gsm8k.txt" &
+    # CUDA_VISIBLE_DEVICES=6  python lmeval_main.py --model "${MODEL}" ${SUFFIX_CMD} --quantize --output "./logs/gsm8k_benchmark/fp4/${MODEL##*/}_gsm8k.jsonl" --task gsm8k_cot_llama 2>&1 | tee "logs/gsm8k_benchmark/fp4/${MODEL##*/}_gsm8k.txt" &
     
     MODEL="Qwen/Qwen3-4B-Thinking-2507"
-    CUDA_VISIBLE_DEVICES=2  python lmeval_main.py --model "${MODEL}" ${SUFFIX_CMD} --quantize --output "./logs/gsm8k_benchmark/fp4/${MODEL##*/}_gsm8k.jsonl" 2>&1 | tee "logs/gsm8k_benchmark/fp4/${MODEL##*/}_gsm8k.txt" &
+    CUDA_VISIBLE_DEVICES=2  python lmeval_main.py --model "${MODEL}" ${SUFFIX_CMD} --quantize --output "./logs/gsm8k_benchmark/fp4/${MODEL##*/}_gsm8k.jsonl" --task gsm8k_cot 2>&1 | tee "logs/gsm8k_benchmark/fp4/${MODEL##*/}_gsm8k.txt" &
 
 elif [ "$MODE" = "kvquant" ] || [ "$MODE" = "all" ]; then
     mkdir -p "logs/gsm8k_benchmark/kvquant/"
@@ -56,15 +56,15 @@ elif [ "$MODE" = "kvquant" ] || [ "$MODE" = "all" ]; then
     
 
     MODEL="meta-llama/Llama-3.2-3B-Instruct"
-    CUDA_VISIBLE_DEVICES=3  python lmeval_main.py --model "${MODEL}" ${SUFFIX_CMD} --kvquant --output "./logs/gsm8k_benchmark/kvquant/${MODEL##*/}_gsm8k.jsonl" 2>&1 | tee "logs/gsm8k_benchmark/kvquant/${MODEL##*/}_gsm8k.txt" 
+    CUDA_VISIBLE_DEVICES=3  python lmeval_main.py --model "${MODEL}" ${SUFFIX_CMD} --kvquant --output "./logs/gsm8k_benchmark/kvquant/${MODEL##*/}_gsm8k.jsonl" --task gsm8k_cot_llama 2>&1 | tee "logs/gsm8k_benchmark/kvquant/${MODEL##*/}_gsm8k.txt" 
     
     MODEL="deepseek-ai/DeepSeek-R1-0528-Qwen3-8B"
-    CUDA_VISIBLE_DEVICES=6  python lmeval_main.py --model "${MODEL}" ${SUFFIX_CMD} --kvquant --output "./logs/gsm8k_benchmark/kvquant/${MODEL##*/}_gsm8k.jsonl" 2>&1 | tee "logs/gsm8k_benchmark/kvquant/${MODEL##*/}_gsm8k.txt" &
+    CUDA_VISIBLE_DEVICES=6  python lmeval_main.py --model "${MODEL}" ${SUFFIX_CMD} --kvquant --output "./logs/gsm8k_benchmark/kvquant/${MODEL##*/}_gsm8k.jsonl" --task gsm8k_cot 2>&1 | tee "logs/gsm8k_benchmark/kvquant/${MODEL##*/}_gsm8k.txt" &
     
     # MODEL="meta-llama/Llama-3.3-70B-Instruct"
-    # CUDA_VISIBLE_DEVICES=6  python lmeval_main.py --model "${MODEL}" ${SUFFIX_CMD} --kvquant --output "./logs/gsm8k_benchmark/kvquant/${MODEL##*/}_gsm8k.jsonl" 2>&1 | tee "logs/gsm8k_benchmark/kvquant/${MODEL##*/}_gsm8k.txt" &
+    # CUDA_VISIBLE_DEVICES=6  python lmeval_main.py --model "${MODEL}" ${SUFFIX_CMD} --kvquant --output "./logs/gsm8k_benchmark/kvquant/${MODEL##*/}_gsm8k.jsonl" --task gsm8k_cot_llama 2>&1 | tee "logs/gsm8k_benchmark/kvquant/${MODEL##*/}_gsm8k.txt" &
     
     # MODEL="Qwen/Qwen3-4B-Thinking-2507"
-    # CUDA_VISIBLE_DEVICES=7  python lmeval_main.py --model "${MODEL}" ${SUFFIX_CMD} --kvquant --output "./logs/gsm8k_benchmark/kvquant/${MODEL##*/}_gsm8k.jsonl" 2>&1 | tee "logs/gsm8k_benchmark/kvquant/${MODEL##*/}_gsm8k.txt" &
+    # CUDA_VISIBLE_DEVICES=7  python lmeval_main.py --model "${MODEL}" ${SUFFIX_CMD} --kvquant --output "./logs/gsm8k_benchmark/kvquant/${MODEL##*/}_gsm8k.jsonl" --task gsm8k_cot 2>&1 | tee "logs/gsm8k_benchmark/kvquant/${MODEL##*/}_gsm8k.txt" &
 
 fi
