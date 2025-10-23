b7ed7df22c36ceeab5f64593ebc94a824a08296c
Tanmaey Gupta <tanmaey@research-common-h200-01.cloud.together.ai>
2025-10-23 21:00:14 +0000

    added setup file

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e  # exit on error
 
-Initialize conda
+
+git submodule update --init --recursive
+
+
 eval "$(conda shell.bash hook)"
 
 # Create env
@@ -11,37 +14,13 @@ conda create -n fp4 python=3.12 -y
 conda activate fp4
 
 # Install packages
-pip install lm_eval==0.4.9.1 matplotlib glog
+pip install matplotlib glog
 
 
-BASHRC="$HOME/.bashrc"
+rm -rf ./third_party/lm-eval/lm_eval/tasks/gpqa
+rm -rf ./third_party/lm-eval/lm_eval/tasks/gsm8k
+rm -rf ./third_party/lm-eval/lm_eval/tasks/gsm8k_platinum
 
-# Remove old CUDA lines (if any)
-sed -i '/export CUDA_HOME=\/usr\/local\/cuda/d' "$BASHRC"
-sed -i '/export PATH=\$CUDA_HOME\/bin:\$PATH/d' "$BASHRC"
-sed -i '/export LD_LIBRARY_PATH=\$CUDA_HOME\/lib64:\$LD_LIBRARY_PATH/d' "$BASHRC"
+cp -r ./tasks/aime/ ./tasks/gpqa/ ./tasks/gsm8k/ ./tasks/gsm8k_platinum/ ./third_party/lm-eval/lm_eval/tasks/
 
-# Add new CUDA 12.9 lines
-cat << 'EOF' >> "$BASHRC"
-
-# >>> CUDA 12.9 default setup >>>
-export CUDA_HOME=/usr/local/cuda
-export PATH=$CUDA_HOME/bin:$PATH
-export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
-# <<< CUDA 12.9 default setup <<<
-EOF
-
-source "$BASHRC"
-# Install local package if directory exists
-if [ -d "fast_fp4" ]; then
-  cd fast_fp4
-  pip install -e .
-else
-  echo "Directory fast_fp4 not found. Skipping local install."
-fi
-
-rm -rf /home/tanmaey/miniconda3/envs/fp4/lib/python3.12/site-packages/lm_eval/tasks/gpqa
-rm -rf /home/tanmaey/miniconda3/envs/fp4/lib/python3.12/site-packages/lm_eval/tasks/gsm8k
-rm -rf /home/tanmaey/miniconda3/envs/fp4/lib/python3.12/site-packages/lm_eval/tasks/gsm8k_platinum
-cd tasks
-cp aime/ gpqa/ gsm8k/ gsm8k_platinum/ /home/tanmaey/miniconda3/envs/fp4/lib/python3.12/site-packages/lm_eval/tasks/
+python -m pip install -e third_party/lm-eval
